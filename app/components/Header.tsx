a1c7d7eb89adeb22a6143fa752c0a37fc17f08fb
Charles Inwald <cinwald@gmail.com>
2024-09-05 04:51:28 -0400

    Update portfolio link

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -6,7 +6,7 @@ export const Header = (
     <div className="fixed max-lg:hidden bottom-0 left-0 flex h-24 sm:h-48 w-full items-end justify-center lg:bg-gradient-to-t from-[#282828] via-[#282828] lg:static lg:size-auto">
       <a
         className="pointer-events-none flex place-items-center gap-2 p-4 sm:p-8 lg:pointer-events-auto lg:p-0 text-[#ebdbb2]"
-        href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+        href="https://charlesinwald.com"
         target="_blank"
         rel="noopener noreferrer"
       >
